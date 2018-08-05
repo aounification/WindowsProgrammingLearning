f30550bcd59ec65bd8445545e8c80195e644572f
Roland Aoun <roland.aoun@murex.com>
2018-08-06 00:11:17 +0300

    Basic screen

---
+++ WindowsProgrammingLearning/Resource.h
@@ -0,0 +1,30 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by WindowsProgrammingLearning.rc
+//
+#define IDC_MYICON                      2
+#define IDD_WINDOWSPROGRAMMINGLEARNING_DIALOG 102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_WINDOWSPROGRAMMINGLEARNING  107
+#define IDI_SMALL                       108
+#define IDC_WINDOWSPROGRAMMINGLEARNING  109
+#define IDS_ABOUT_TEXT                  110
+#define IDR_MAINFRAME                   128
+#define IDI_ICON1                       130
+#define IDI_BIGSAILPRODUCTION           130
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_SYMED_VALUE           111
+#endif
+#endif
